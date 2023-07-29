eced33c56e5595b7871e3a6849984f90cb616dcb
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-07-29 15:23:03 -0700

    fixed empty path error

--- main.c
+++ main.c
@@ -133,17 +133,6 @@ int _ch(char **argv, char **tokens, char **fullpath, int *exit_status)
 	char newline = '\n';
 	char *path = _getenv("PATH");
 
-	if (path == NULL || *path == '\0')
-	{
-		write(STDERR_FILENO, argv[0], strlen(argv[0]));
-		write(STDERR_FILENO, ": 1: ", 5);
-		write(STDERR_FILENO, tokens[0], strlen(tokens[0]));
-		write(STDERR_FILENO, ": not found", 11);
-		write(STDERR_FILENO, &newline, 1);
-		*exit_status = 127;
-		return (2);
-	} /* end if */
-
 	if (access(tokens[0], X_OK) == -1)
 	{
 		*fullpath = _which(tokens[0], path);
